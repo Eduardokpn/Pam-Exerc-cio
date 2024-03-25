0fa9c041db90b4e501c8080b9354eb1df75d1de6
Edu <eduardokaue277@gmail.com>
2024-03-25 20:45:59 -0300

    Feat: Upd

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void UpperBoundRandoms(object sender, EventArgs e)
+        {
+            var seed = 0;
+            var upper = 10;
+            Random randObj = new Random(seed);
+            randObj.Next(upper);
+
+        }
+
+
+    }
+
+}
